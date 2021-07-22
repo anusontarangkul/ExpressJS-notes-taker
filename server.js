cbcbdc58be4f70a53d7f300b9488defe910ae3d9
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-07-21 20:03:34 -0700

    add htmlRoutes

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 // app.use(express.json());
 // app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+app.use('/', htmlRoutes);
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}.`)
