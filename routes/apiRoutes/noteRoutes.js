2365e8b169061b562c994021586f941dc1bebd99
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-07-21 23:18:44 -0700

    delete note route

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,16 +1,32 @@
-const db = require('../../db/db.json');
+const { noteDB } = require('../../db/db.json');
 const router = require('express').Router();
-const { createNote } = require('../../lib/notes')
+const { createNote, createNoteFromDelete } = require('../../lib/notes')
 
 router.get('/notes', (req, res) => {
-    console.log(db)
-    res.json(db)
+    console.log(noteDB)
+    res.json(noteDB)
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = db.length.toString();
-    const newNote = createNote(req.body, db);
+    req.body.id = noteDB.length.toString();
+
+    const newNote = createNote(req.body, noteDB);
     res.json(newNote)
+});
+
+router.delete('/notes/:id', (req, res) => {
+
+    const noteIndex = req.params.id;
+
+    const noteObj = noteDB.find(note => note.id === noteIndex);
+
+    let deletenoteIndex = noteDB.indexOf(noteObj);
+    console.log('before delete')
+    console.log(noteDB)
+    noteDB.splice(deletenoteIndex, 1);
+    let newNotesDB = noteDB.filter(value => Object.keys(value).length !== 0);
+    const newNote = createNoteFromDelete(newNotesDB);
+    res.send(noteObj);
 })
 
 module.exports = router;
